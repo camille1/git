fa3ea3a32bd1ed20b4fa6dc61b767fd12f14a481
camille1 <camillelacan1@0gmail.com>
2016-02-05 19:47:33 +0100

    d

---
+++ Replication.r
@@ -0,0 +1,4 @@
+## The Reproducible R Toolkit (RRT)
+library(checkpoint)
+## year-month-day
+checkpoint("2016-02-05")
--- magrittr.R
+++ magrittr.R
@@ -1,1 +1,12 @@
 library(magrittr)
+
+
+data1<- mtcars %>%
+  subset(hp > 100) %>%
+  aggregate(. ~ cyl, data = ., FUN = . %>% mean %>% round(2)) %>%
+  transform(kpl = mpg %>% multiply_by(0.4251)) %>%
+  print
+
+data2 <- airquality %>%
+  summary() %>%
+  print
